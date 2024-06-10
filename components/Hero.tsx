f37558bfcaa7c63bca28ae53cd6ee03faac4af22
Isabel <isabel.moreno@genstudents.org>
2024-06-10 13:40:55 -0500

    v0

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -31,20 +31,20 @@ const Hero = () => {
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
           <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-            Dynamic Web Magic with Next.js
+            Portafolio de servicios
           </p>
           <TextGenerateEffect
-            words="Transforming Concepts into Seamless User Experiences"
+            words="Transformando Conceptos En Experiencias De Usuario Fluidas"
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi! I&apos;m Adrian, a Next.js Developer based in Croatia.
+            Hola! soy Isabel Moreno, Desarrolladora Fullstack | Diseñadora | Digital Marketer
           </p>
 
           <a href="#about">
             <MagicButton
-              title="Show my work"
+              title="Mis proyectos"
               icon={<FaLocationArrow />}
               position="right"
             />

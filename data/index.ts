98514c19094f3918996d3ca682a02046b9d93a6a
Isabel <isabel.moreno@genstudents.org>
2024-06-10 16:00:11 -0500

    removing unused stuff

--- data/index.ts
+++ data/index.ts
@@ -47,7 +47,7 @@ export const navItems = [
       spareImg: "/b4.svg",
     },
   
-    {
+/*     {
       id: 5,
       title: "Currently building a JS Animation library",
       description: "The Inside Scoop",
@@ -56,7 +56,7 @@ export const navItems = [
       titleClassName: "justify-center md:justify-start lg:justify-center",
       img: "/b5.svg",
       spareImg: "/grid.svg",
-    },
+    }, */
     {
       id: 6,
       title: "Do you want to start a project together?",
@@ -105,7 +105,7 @@ export const navItems = [
   ];
   
   export const testimonials = [
-    {
+/*     {
       quote:
         "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
       name: "Michael Johnson",
@@ -134,7 +134,7 @@ export const navItems = [
         "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
       name: "Michael Johnson",
       title: "Director of AlphaStream Technologies",
-    },
+    }, */
   ];
   
   export const companies = [
